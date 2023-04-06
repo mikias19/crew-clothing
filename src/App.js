1b370a272e8f7ed01dd2b45752334e405d9a51ab
mikias <mikikebe84@gmail.com>
2023-04-06 14:18:22 +0300

    adding Heading Component the link for the image , shop and conatct

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { HomePage } from "./Componets/pages/homepages/homepage.jsx";
 import { Route, Routes } from "react-router-dom";
 import { ShopPage } from "./Componets/pages/shop/shop";
+import Header from "./Componets/header/header";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
